ecced214928c10d39ffb22d46d5cacee63e6b39d
Yeiber Cano <yeibercano@gmail.com>
2016-07-12 02:38:56 -0700

    add validation to form with redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index'; 
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
 
@@ -11,28 +12,58 @@ class PostsNew extends Component {
 		return (
 			<form onSubmit={handleSubmit(this.props.createPost)}>
 				<h3>Create A New Post</h3>
-				<div className='form-group>'>
+				<div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
 					<label>Title</label>
 					<input type='text' className='form-control' {...title}/>
+					<div className='text-help'>
+						{ title.touched ? title.error : '' }
+					</div>
 				</div>
 
-				<div className='form-group>'>
+				<div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
 					<label>Categories</label>
 					<input type='text' className='form-control' {...categories}/>
+					<div className='text-help'>
+						{ categories.touched ? categories.error : '' }
+					</div>
 				</div>
 
-				<div className='form-group>'>
+				<div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
 					<label>Content</label>
 					<textarea className='form-control' {...content}/>
+					<div className='text-help'>
+						{ content.touched ? content.error : '' }
+					</div>
 				</div>
 
 				<button type='submit' className='btn btn-primary'>Submit</button>
+				<Link to='/' className='btn btn-danger'> Cancel </Link>
+
 		  </form>		
 		)
 	}
 }
 
+function validate(values) {
+	const errors = {};
+
+	if (!values.title) {
+		errors.title ='Enter a title';
+	}
+	if (!values.categories) {
+		errors.categories ='Enter categories';
+	}
+	if (!values.content) {
+		errors.content ='Enter some content';
+	}
+
+	return errors
+}
+
 export default reduxForm({
 	form:'PostsNewForm',
-	fields: ['title', 'categories', 'content']
+	fields: ['title', 'categories', 'content'],
+	validate
 }, null, { createPost })(PostsNew);
+
+
