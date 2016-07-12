b8c9a63877735aeccaad5e47f6d7f52d7b9fa452
Yeiber Cano <yeibercano@gmail.com>
2016-07-12 01:59:14 -0700

    add createPost saving formData to database

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
+
 
 export const ROOT_URL ='http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=fsdfdbgjbdljbdfgdsblbdsafsd';
@@ -12,4 +14,13 @@ export function fetchPosts() {
 		type: FETCH_POSTS,
 		payload: request
 	}
+}
+
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+  	type: CREATE_POST,
+  	payload: request
+  }
 }
