9fe99e107c906a7dc3be3044465d982a690af7dc
Yeiber Cano <yeibercano@gmail.com>
2016-07-12 00:20:51 -0700

    refactor to container to make redux connection

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'; //delete the commented out too
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-	return <div> Posts </div>
-} 
+class PostsIndex extends Component {
+	componentWillMount() {
+   this.props.fetchPosts();
+	}
+
+	render() {
+		return (
+			<div>List of blog posts</div>
+		);
+	}
+}
+
+// function mapDispatchToProps(dispatch) {
+// 	return bindActionCreators({ fetchPosts }, dispatch);
+
+// }
+
+// export default connect(null, mapDispatchToProps)(PostsIndex);
+
+//refactor version
+export default connect(null, { fetchPosts })(PostsIndex);
