7ede6bfa1adb8e507466c51ee97c0073fc0b028d
Yeiber Cano <yeibercano@gmail.com>
2016-07-12 13:55:19 -0700

    add post route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
+import PostsShow from './components/posts_show';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
@@ -9,6 +10,7 @@ export default (
   <Route path='/' component={App}>
   	<IndexRoute component={PostsIndex} />
   	<Route path='posts/new' component={PostsNew} />
+  	<Route path='posts/:id' component={PostsShow} />
   </Route>
 
 );
