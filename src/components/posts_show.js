ab3a6bbda16d0ec186ac4a7183b159bb4259a9f1
Yeiber Cano <yeibercano@gmail.com>
2016-07-12 14:20:17 -0700

    fetch post id

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component {
+	componentWillMount() {
+		this.props.fetchPost(this.props.params.id);
+	}
+
 	render() {
 		return (
 			<div>{this.props.params.id}</div>
@@ -9,4 +15,5 @@ class PostsShow extends Component {
 	}
 }
 
-export default PostsShow;
+
+export default connect(null, { fetchPost } )(PostsShow);
