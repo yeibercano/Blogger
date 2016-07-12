ddf092d15db4021f8a203e2ebbeac781e696858b
Yeiber Cano <yeibercano@gmail.com>
2016-07-12 14:32:02 -0700

    map state to props and render current post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -8,12 +8,26 @@ class PostsShow extends Component {
 	}
 
 	render() {
-		return (
-			<div>{this.props.params.id}</div>
+		const { post } = this.props;
 
-		)
+		if(!post) {
+			return <div>Loading...</div>
+		}
+
+		return (
+			<div> 
+				<h3>{post.title}</h3>
+				<h6>Categories: { post.categories } </h6>
+				<p>{post.content}</p>
+			</div>
+		);
+	}
+};
+
+function mapStateToProps(state) {
+	return {
+		post: state.posts.post
 	}
 }
 
-
-export default connect(null, { fetchPost } )(PostsShow);
+export default connect(mapStateToProps, { fetchPost } )(PostsShow);
