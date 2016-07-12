a48e82c5d68b633938cbe91b3453ed4b1b67a711
Yeiber Cano <yeibercano@gmail.com>
2016-07-11 22:29:05 -0700

    import routes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 
+import routes from './routes';
+
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={ browserHistory } />
+    <Router history={ browserHistory } routes={routes} />
   </Provider>
   , document.querySelector('.container'));
