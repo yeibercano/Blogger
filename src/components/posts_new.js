7aeac3494504f7f551157f239c7899b58fba6a17
Yeiber Cano <yeibercano@gmail.com>
2016-07-12 01:40:18 -0700

    pass down props to input with spread operator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,25 +2,31 @@ import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+	
 	render() {
+	  const { fields: {title, categories, content }, handleSubmit } = this.props;
+
 		return (
-			<h3>Create A New Post</h3>
-			<div className='form-group>'>
-				<label>Title</label>
-				<input type='text' className='form-control'/>
-			</div>
+			<form onSubmit={handleSubmit}>
+				<h3>Create A New Post</h3>
+				<div className='form-group>'>
+					<label>Title</label>
+					<input type='text' className='form-control' {...title}/>
+				</div>
 
-			<div className='form-group>'>
-				<label>Categories</label>
-				<input type='text' className='form-control'/>
-			</div>
+				<div className='form-group>'>
+					<label>Categories</label>
+					<input type='text' className='form-control' {...categories}/>
+				</div>
 
-			<div className='form-group>'>
-				<label>Content</label>
-				<textarea className='form-control'/>
-			</div>
+				<div className='form-group>'>
+					<label>Content</label>
+					<textarea className='form-control' {...content}/>
+				</div>
 
-			<button type='submit' className='btn btn-primary'>Submit</button>
+				<button type='submit' className='btn btn-primary'>Submit</button>
+		  </form>		
 		)
 	}
 }
